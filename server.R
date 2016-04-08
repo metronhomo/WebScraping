e69f18021b114a84ebd16ffadce635592bd8a4dc
Mario <mariob.c@hotmail.com>
2016-04-08 11:14:47 -0500

    side panel y un solo botón de descarga de selección

--- server.R
+++ server.R
@@ -20,17 +20,61 @@ shinyServer(function(input, output) {
     return(a)
   })
   
-#   output$openprice <- renderGvis({
-#     gvisTable(data_openprice())
-#   })
+  #   output$openprice <- renderGvis({
+  #     gvisTable(data_openprice())
+  #   })
   
-#   output$openprice <- renderTable({
-#     data_openprice()
-#   })
+  #   output$openprice <- renderTable({
+  #     data_openprice()
+  #   })
   
   output$openprice <- renderDataTable(
     data_openprice(),
     options = list(pageLength = 150)
+  )
+  
+  dataset_Input <- reactive({
+    descarga_tienda <- unlist(input$opciones_descarga_tienda)
+    descarga_lev <- unlist(input$opciones_descarga_levantamiento)
+    return(list(tienda = descarga_tienda,
+                lev = descarga_lev))
+  })
+  
+  output$download_sel <- downloadHandler(
+    filename = function() {
+      paste('precios-', Sys.Date(), '.zip', sep='')
+    },
+    content = function(fname) {
+      descarga_tienda <- dataset_Input()[[1]]
+      descarga_lev <- dataset_Input()[[2]]
+      
+      setwd(tempdir())
+      
+      if(sum(grepl("Elektra", descarga_tienda)) > 0){
+        productos_ekt = productos %>% 
+          filter(Tienda == "Elektra") %>% 
+          select(-Tienda)
+        write.csv(productos_ekt, file = "Elektra.csv")
+      }
+      
+      if(sum(grepl("Famsa", descarga_tienda)) > 0){
+        productos_famsa = productos %>% 
+          filter(Tienda == "Famsa") %>% 
+          select(-Tienda)
+        write.csv(productos_famsa, file = "Famsa.csv")
+      }
+      
+      if(sum(grepl("Coppel", descarga_tienda)) > 0){
+        productos_coppel = productos %>% 
+          filter(Tienda == "Coppel") %>% 
+          select(-Tienda)
+        write.csv(productos_coppel, file = "Coppel.csv")
+      }
+      
+      zip(zipfile = fname, files = paste0(descarga_tienda, ".csv"))
+      if(file.exists(paste0(fname, ".zip"))) file.rename(paste0(fname, ".zip"), fname)
+    },
+    contentType = "application/zip"
   )
   
   output$download_EKT <- downloadHandler(
--- ui.R
+++ ui.R
@@ -6,19 +6,62 @@ shinyUI(
     "Comparación de precios",
     theme = shinytheme("flatly"),
     tabPanel("Open Price",
-             textOutput("texto"),
-             radioButtons('filtroProducto', 
-                     label = '',
-                     choices = unique(productos$Producto),
-                     selected = unique(productos$Producto)[1],
-                     inline = T
-                     ),
-             mainPanel(
-               dataTableOutput("openprice"),
-               downloadButton('download_EKT', 'Descargar datos de Elektra'),
-               downloadButton('download_Coppel', 'Descargar datos de Coppel'),
-               downloadButton('download_Famsa', 'Descargar datos de Famsa')
-             )
+             column(3,
+                    textOutput("texto"),
+                    br(),
+                    selectInput("levantamiento", 
+                                "Elige el levantamiento",
+                                choices = c("Levantamiento 1", "Levantamiento 2"),
+                                selected = "Levantamiento 2"
+                                ), # End SelectInput
+                    wellPanel(style = "background-color: #2c3e50;",
+                      h3('¿Qué significa?',
+                         align="center",
+                         style = "color:#C2D1E0"),
+                      br(),
+                      h4("La tabla muestra el precio de apertura más bajo para cada tienda y para cada clasificación de producto.
+                         Se puede elegir el producto que se quiere ver, y la fecha en la cual se obtuvieron los datos.",
+                         style = "color:#C2D1E0"),
+                      h4("Además, se tiene la opción de descargar los datos de cualquier fecha, producto y tienda.",
+                         style = "color:#C2D1E0")
+                    ), # End wellPanel
+                    wellPanel(
+                      h3("Selecciona las opciones para descargar los datos:"),
+                      checkboxGroupInput("opciones_descarga_tienda",
+                                         "Tiendas",
+                                         choices = list("Coppel",
+                                                        "Elektra",
+                                                        "Famsa"),
+                                         selected = list("Coppel",
+                                                         "Elektra",
+                                                         "Famsa"),
+                                         inline = T
+                                         ),
+                      checkboxGroupInput("opciones_descarga_levantamiento",
+                                         "Levantamiento",
+                                         choices = list("Levantamiento 1",
+                                                        "Levantamiento 2"),
+                                         selected = "Levantamiento 2",
+                                         inline = T
+                      ),
+                      downloadButton('download_sel', 'Descargar selección')
+                    ) # end wellPanel
+             ), # End column
+             column(8,
+                    radioButtons('filtroProducto', 
+                                 label = '',
+                                 choices = unique(productos$Producto),
+                                 selected = unique(productos$Producto)[1],
+                                 inline = T
+                    ),
+                    mainPanel(
+                      dataTableOutput("openprice")
+#                       downloadButton('download_EKT', 'Descargar datos de Elektra'),
+#                       downloadButton('download_Coppel', 'Descargar datos de Coppel'),
+#                       downloadButton('download_Famsa', 'Descargar datos de Famsa'),
+                      
+                    ) # End mainPanel
+             ) # End column
     )
     
   )
