0c7ef14a05d24aa932f0cd76743f9b6b8bbae8d0
Mario <mariob.c@hotmail.com>
2016-04-28 13:17:07 -0500

    filtra productos con levantamientos de las 3 tiendas

--- global.R
+++ global.R
@@ -7,20 +7,39 @@ library(scales)
 
 source("helpers.R")
 
-olas_folder <- "data/olas/"
-archivos <- paste0(olas_folder, list.files(olas_folder))
+# Indica folder donde están los archivos rds con los datos de los levantamientos
+olas_folder <- "data/olas"
 
+# Guarda los archivos en un vector
+archivos <- list.files(olas_folder, full.names = T)
 
+# Lee todos los archivos y luego los convierte en un solo dataframe
 productos <- lapply(archivos, function(x){
   readRDS(x) %>% 
     unique(.) %>% 
     mutate(Precio = as.numeric(Precio))
 }) %>% rbind_all()
 
+# Encuentra qué levantamientos son válidos para todas las tiendas.
+# Por ejemplo, si en el levantamiento 3 no está incluida Famsa,
+# entonces el vector levantamientos_num no contendrá al levantamiento 3
+levantamientos_num <- productos %>% 
+  group_by(Levantamiento, Tienda) %>% 
+  tally() %>% 
+  select(Levantamiento) %>% 
+  tally() %>% 
+  filter(n == 3) %>% 
+  .$Levantamiento
 
-levantamientos <- paste("Levantamiento", sort(unique(productos$Levantamiento), 
-                                              decreasing = T))
+# Filtra los productos para que solo tengan los levantamientos completos
+productos <- productos %>% 
+  filter(Levantamiento %in% levantamientos_num)
 
+# Vector que aparecerá en las opciones del Shiny
+levantamientos <- paste("Levantamiento", sort(levantamientos_num, 
+                                               decreasing = T))
+
+#productos_list <- unique(productos$Producto)
 productos_list <- list("Lavadoras" = "LAVADORA",
                        "Refrigeradores" = "REFRIGERADOR",
                        "Pantallas" = "PANTALLA",
